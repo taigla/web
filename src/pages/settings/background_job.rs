4d88fd7500b48759a58395cab4ba8a064f12af91
gaetan <gaetan.depin@epitech.eu>
2023-11-04 09:45:54 +0100

    Add background job view

--- src/pages/settings/background_job.rs
+++ src/pages/settings/background_job.rs
@@ -1,6 +1,131 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
+use serde::Deserialize;
+use crate::hooks::use_swr::{use_swr, State};
+
+#[derive(Deserialize)]
+pub enum WorkerState {
+    Idle,
+    #[serde(untagged)]
+    Running {
+        job: String
+    }
+}
+
+#[derive(Deserialize)]
+struct Job {
+    uuid: String,
+    interval: u32,
+    name: String
+}
+
+#[inline_props]
+fn WorkerList<'a>(cx: Scope, workers: &'a Vec<WorkerState>) -> Element {
+    let rows = workers.iter().map(|worker| {
+        match worker {
+            WorkerState::Idle => {
+                rsx! {
+                    tr {
+                        // key: "{job.uuid}",
+                        td { "Idle" }
+                        td { "" }
+                    }
+                }
+            },
+            WorkerState::Running { job } => {
+                rsx! {
+                    tr {
+                        // key: "{job.uuid}",
+                        td { "Running" }
+                        td { "{job}" }
+                    }
+                }
+            }
+        }
+    });
+
+    render! {
+        table {
+            class: "table bordered",
+            thead {
+                tr {
+                    th { class: "w-1/12", "Status" }
+                    th { class: "w-11/12", "Job" }
+                }
+            }
+            tbody {
+                rows
+            }
+        }
+    }
+}
+
+fn Workers(cx: Scope) -> Element {
+    let workers = use_swr::<Vec<WorkerState>>(cx, "/api/v1/job/workers");
+
+    render! {
+        div {
+            p { class: "text-xl", "Worker" }
+            match workers {
+                State::Ok(w) => rsx! { WorkerList { workers: &w } },
+                _ => rsx! { "Loading" }
+            }
+        }
+    }
+}
+
+#[inline_props]
+fn CronjobList<'a>(cx: Scope, jobs: &'a Vec<Job>) -> Element {
+    let rows = jobs.iter().map(|job| {
+        rsx! {
+            tr {
+                key: "{job.uuid}",
+                td { "{job.name}" }
+                td { "{job.interval}" }
+            }
+        }
+    });
+
+    render! {
+        table {
+            class: "table bordered",
+            thead {
+                tr {
+                    th { class: "w-11/12", "Name" }
+                    th { class: "w-1/12", "Interval" }
+                }
+            }
+            tbody {
+                rows
+            }
+        }
+    }
+}
+
+fn Cronjobs(cx: Scope) -> Element {
+    let cronjobs = use_swr::<Vec<Job>>(cx, "/api/v1/job/cronjobs");
+
+    render! {
+        div {
+            p { class: "text-xl", "Cronjob" }
+            match cronjobs {
+                State::Ok(j) => rsx! { CronjobList { jobs: &j } },
+                _ => rsx! { "Loading" }
+            }
+        }
+    }
+}
 
 pub fn BackgroundJobs(cx: Scope) -> Element {
-    render! { "Background jobs" }
+    render! {
+        div {
+            class: "flex flex-col w-full",
+            div {
+                class: "flex flex-row justify-between pb-2",
+                p { class: "text-2xl", "Background jobs" }
+            }
+            Workers {}
+            Cronjobs {}
+        }
+    }
 }
