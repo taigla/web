66be024f638c8deaa2d874b96179f5c20ea97822
gaetan <gaetan.depin@epitech.eu>
2023-11-08 07:39:42 +0100

    Improve use query Add don't resent cached request Mutability Unregister query listener on drop of use query

--- src/hooks/use_query.rs
+++ src/hooks/use_query.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 use super::use_query_provider::{UseQueryProvider, use_query_provider, QueryValue, RegistryEntry, ValueHash};
 
 #[derive(Debug)]
@@ -14,17 +14,52 @@ pub enum QueryState<T> {
     UserError
 }
 
-#[derive(Debug)]
 pub struct UseQuery<T: DeserializeOwned> {
     pub value: QueryState<T>,
-    pub mutate: u32,
     value_hash: ValueHash,
-    registry_entry: RegistryEntry
+    registry_entry: RegistryEntry,
+    query_provider: UseQueryProvider,
+    scope_id: ScopeId
 }
 
 impl<T: DeserializeOwned> UseQuery<T> {
-    pub fn new(provider: &UseQueryProvider, url: &str, scope_id: ScopeId) -> Self {
-        let registry_entry = provider.add_listener(url, scope_id);
+    fn check_update(&mut self) {
+        let readable_entry = self.registry_entry
+            .read()
+            .unwrap();
+        if self.value_hash != readable_entry.hash {
+            log::info!("Update data");
+            let query_value = readable_entry
+                .value
+                .clone();
+
+            self.value = match query_value {
+                QueryValue::Ok(v) => QueryState::Ok(serde_json::from_value::<T>(v).unwrap()),
+                QueryValue::Validating(v) => QueryState::Validating(serde_json::from_value::<T>(v).unwrap()),
+                QueryValue::Error => QueryState::Error,
+                QueryValue::Loading => QueryState::Loading,
+                QueryValue::NotFetch => QueryState::Loading
+            };
+            self.value_hash = readable_entry.hash;
+        }
+    }
+
+    pub fn mutate<U: Serialize>(&self, new_value: U) {
+        self.query_provider.mutate(&self.registry_entry, serde_json::to_value(new_value).unwrap());
+    }
+}
+
+impl<T: DeserializeOwned> Drop for UseQuery<T> {
+    fn drop(&mut self) {
+        self.query_provider.remove_listener(&self.registry_entry, self.scope_id);
+    }
+}
+
+pub fn use_query<'a, T: DeserializeOwned + 'static>(cx: &'a ScopeState, url: &str) -> &'a UseQuery<T> {
+    let provider = use_query_provider(&cx);
+
+    let hook = cx.use_hook(|| {
+        let registry_entry = provider.add_listener(url, cx.scope_id());
         let readable_entry = registry_entry
             .read()
             .unwrap();
@@ -38,49 +73,29 @@ impl<T: DeserializeOwned> UseQuery<T> {
             QueryValue::Ok(v) => QueryState::Ok(serde_json::from_value::<T>(v).unwrap()),
             QueryValue::Validating(v) => QueryState::Validating(serde_json::from_value::<T>(v).unwrap()),
             QueryValue::Error => QueryState::Error,
-            QueryValue::Loading => QueryState::Loading
-        };
-        Self {
-            value: value,
-            mutate: 0,
-            value_hash: hash,
-            registry_entry: registry_entry
-        }
-    }
-
-    fn check_update(&mut self) {
-        let readable_entry = self.registry_entry
-            .read()
-            .unwrap();
-        if self.value_hash != readable_entry.hash {
-            let query_value = readable_entry
-                .value
-                .clone();
-
-            self.value = match query_value {
-                QueryValue::Ok(v) => QueryState::Ok(serde_json::from_value::<T>(v).unwrap()),
-                QueryValue::Validating(v) => QueryState::Validating(serde_json::from_value::<T>(v).unwrap()),
-                QueryValue::Error => QueryState::Error,
-                QueryValue::Loading => QueryState::Loading
-            };
-            self.value_hash = readable_entry.hash;
-        }
-    }
-}
-
-pub fn use_query<'a, T: DeserializeOwned + 'static>(cx: &'a ScopeState, url: &str) -> &'a UseQuery<T> {
-    let provider = use_query_provider(&cx);
-
-    let hook = cx.use_hook(|| {
-        log::info!("New use query");
-        let query = UseQuery::<T>::new(&provider, url, cx.scope_id());
-
-        cx.spawn({
-            to_owned![url, provider];
-            async move {
-                provider.update(&url).await;
+            QueryValue::Loading => QueryState::Loading,
+            QueryValue::NotFetch => {
+                let mut writable_entry = registry_entry
+                    .write()
+                    .unwrap();
+                writable_entry.value = QueryValue::Loading;
+                cx.spawn({
+                    to_owned![url, provider];
+                    async move {
+                        provider.update(&url).await;
+                    }
+                });
+                QueryState::Loading
             }
-        });
+        };
+
+        let query = UseQuery::<T> {
+            value: value,
+            value_hash: hash,
+            registry_entry: registry_entry,
+            query_provider: provider.clone(),
+            scope_id: cx.scope_id()
+        };
         query
     });
     hook.check_update();
