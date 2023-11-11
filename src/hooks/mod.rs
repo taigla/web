47087a8886b3bbb3534f873454060822c3cb65de
gaetan <gaetan.depin@epitech.eu>
2023-11-11 08:15:40 +0100

    Add mutate hook for use query

--- src/hooks/mod.rs
+++ src/hooks/mod.rs
@@ -3,8 +3,11 @@ pub use use_query::{use_query, UseQuery, QueryState};
 pub use use_query_provider::{
     Fetcher,
     UseQueryProvider,
-    use_init_query_provider, use_query_provider, QueryError};
+    use_init_query_provider, use_query_provider, QueryError
+};
+pub use use_mutate::{use_mutate, UseMutate};
 
 mod use_taigla_api;
 mod use_query;
 mod use_query_provider;
+mod use_mutate;
