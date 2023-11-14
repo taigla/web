774a0d8e9355e42bef55ee814e44eedcb923cabf
Gaetan <gaetan.depin@epitech.eu>
2023-11-14 17:00:36 +0100

    Refactor login page

--- src/components/ui/input.rs
+++ src/components/ui/input.rs
@@ -2,13 +2,18 @@ use dioxus::prelude::*;
 
 #[inline_props]
 #[allow(non_snake_case)]
-pub fn Input<'a>(cx: Scope, name: &'a str, lbl: Option<&'a str>, default_value: Option<&'a str>) -> Element<'a> {
+pub fn Input<'a>(cx: Scope, name: &'a str, lbl: Option<&'a str>, default_value: Option<&'a str>, r#type: Option<&'a str>) -> Element<'a> {
     render! {
         Fragment {
             if let Some(lbl) = *lbl {
                 rsx! { label { class: "col-span-12 md:col-span-3", lbl } }
             }
-            rsx! { input { class: "input input-bordered col-span-12 md:col-span-9", initial_value: *default_value, name: *name } }
+            input {
+                class: "input input-bordered col-span-12 md:col-span-9",
+                initial_value: *default_value,
+                r#type: *r#type,
+                name: *name
+            }
         }
     }
 }
--- src/pages/auth/login.rs
+++ src/pages/auth/login.rs
@@ -5,41 +5,72 @@ use web_sys::window;
 use crate::hooks::use_taigla_api;
 use crate::routes::Routes;
 use crate::api::{Token, LoginParam};
+use crate::icons::TaiglaLogo;
 
 pub fn Login(cx: Scope) -> Element {
     let navigator = use_navigator(&cx);
     let token = use_shared_state::<Token>(cx).unwrap();
     let api = use_taigla_api(cx);
+    let error = use_state::<Option<String>>(cx, || None);
+    let loading = use_state::<bool>(cx, || false);
+
+    let on_submit = move |event: Event<FormData>| {
+        cx.spawn({
+            to_owned![navigator, token, api, error, loading];
+            async move {
+                log::info!("{:?}", event);
+                loading.set(true);
+                let username: &str = event.data.values.get("name").unwrap().get(0).unwrap();
+                let password: &str = event.data.values.get("password").unwrap().get(0).unwrap();
+                let response = api.read().post_login(&LoginParam { username, password })
+                    .await;
+                match response {
+                    Ok(response) => {
+                        let local_storage = window()
+                            .unwrap()
+                            .local_storage()
+                            .unwrap()
+                            .unwrap();
+                        local_storage.set_item("token", &response.token).unwrap();
+                        log::info!("{}", response.token);
+                        token.write().set(&response.token);
+                        navigator.replace(Routes::Home {});
+                    },
+                    Err(e) => { error.set(Some(e.err_code)) }
+                }
+                loading.set(false);
+            }
+        })
+    };
 
     render! {
-        "Login"
-        form {
-            onsubmit: move |event| {
-                cx.spawn({
-                    to_owned![navigator, token, api];
-                    async move {
-                        log::info!("{:?}", event);
-                        let username: &str = event.data.values.get("name").unwrap().get(0).unwrap();
-                        let password: &str = event.data.values.get("password").unwrap().get(0).unwrap();
-                        let response = api.read().post_login(&LoginParam { username, password })
-                            .await;
-                        if let Ok(response) = response {
-                            let local_storage = window()
-                                .unwrap()
-                                .local_storage()
-                                .unwrap()
-                                .unwrap();
-                            local_storage.set_item("token", &response.token).unwrap();
-                            log::info!("{}", response.token);
-                            token.write().set(&response.token);
-                            navigator.replace(Routes::Home {});
-                        }
+        div {
+            class: "modal modal-open absolute top-0 left-0 bottom-0 right-0",
+            div {
+                class: "modal-backdrop"
+            }
+            div {
+                class: "modal-box max-w-none w-[32rem]",
+                form {
+                    class: "flex flex-col",
+                    onsubmit: on_submit,
+                    div {
+                        class: "flex flex-row items-center justify-center mb-6",
+                        TaiglaLogo { width: 100, height: 100 }
+                        p { class:"font-semibold text-4xl", "Taigla" }
                     }
-                })
-            },
-            input { name: "name" },
-            input { name: "password", r#type: "password" }
-            input { r#type: "submit", value: "Login" }
+                    label { "Name:" }
+                    input { class: "input input-bordered", name: "name" },
+                    label { class: "mt-4", "Password:" }
+                    input { class: "input input-bordered", name: "password", r#type: "password" }
+                    if *error != None { rsx! { p { class: "text-error mt-4", "Invalid user/password" } } } else { rsx! { "" } }
+                    button {
+                        class: "btn btn-primary mt-4",
+                        disabled: **loading,
+                        if **loading { rsx! { span { class: "loading loading-spinner" } } } else { rsx! { "Login" } }
+                    }
+                }
+            }
         }
     }
 }
