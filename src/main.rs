85fc99e925a60605002fe798f26859eb6ad87d68
Gaetan <gaetan.depin@epitech.eu>
2023-11-20 14:46:37 +0100

    Add request lib + log

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
+use log::LevelFilter;
 use taigla::routes::Routes;
 
 fn main() {
+    dioxus_logger::init(LevelFilter::Info).expect("Failed to init logger");
     dioxus_web::launch(App);
 }
 
