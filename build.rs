a3b29064413541d3fe849be84bec80f865616b1c
gaetan <gaetan.depin@epitech.eu>
2023-12-27 20:23:35 +0100

    Fix build when only python3 exist

--- build.rs
+++ build.rs
@@ -1,13 +1,17 @@
 use std::env;
-use std::process::Command;
+use std::process::{Command, Output};
+
+fn run_icon(python_bin: &str) -> std::io::Result<Output> {
+    Command::new(python_bin)
+        .arg("svg_to_dioxus.py")
+        .output()
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=icons/");
-    Command::new("python")
-        .arg("svg_to_dioxus.py")
-        .output()
-        .unwrap();
+    let _ = run_icon("python")
+        .map_err(|_| run_icon("python3").unwrap());
     match env::var("TAIGLA_BACKEND_URL") {
         Ok(b) => println!("cargo:rustc-env=TAIGLA_BACKEND_URL={b}"),
         Err(_) => println!("cargo:rustc-env=TAIGLA_BACKEND_URL=")
